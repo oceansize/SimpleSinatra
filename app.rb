431c598445f717d5b73ae82e349bfdd89ae1a5ef
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-15 11:18:50 +0100

    GET, POST, PUT and DELETE examples

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
-
 class App < Sinatra::Base
   get '/' do
     "Hello World"
+  end
+
+  post '/' do
+    "Hello World, via POST"
+  end
+
+  put '/' do
+    "Hello World, via PUT"
+  end
+
+  delete '/' do
+    "Goodbye Word, via DELETE"
   end
 end
