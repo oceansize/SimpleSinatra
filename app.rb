2fa1516b28d1b3a5a4019b1adda5529dda3c064c
Roi Driscoll <roi.driscoll@gmail.com>
2014-06-15 17:24:01 +0100

    Tidy up code and make options for both erb and haml before ex5

--- app.rb
+++ app.rb
@@ -9,11 +9,7 @@ IMAGES = [
 class App < Sinatra::Base
 
   before do
-    puts "==> Entering Request"
-  end
-
-  after do
-    puts "<== Leaving Request"
+    @user = 'Roi Driscoll'
   end
 
   get '/images/?' do
@@ -21,36 +17,37 @@ class App < Sinatra::Base
     erb :images
   end
 
-  get '/images/:index?' do |index|
+  get '/images/:index' do |index|
     index = index.to_i
     @image = IMAGES[index]
-    # this lets a folder be referenced for the template
     haml :"images/show", layout: true
+    # this lets a folder be referenced for the template
+    #haml :"images/show", layout: true
   end
 
-  get '/' do
-    "Hello World"
-  end
+#   get '/' do
+#     "Hello World"
+#   end
 
-  post '/' do
-    "Hello World, via POST"
-  end
+#   post '/' do
+#     "Hello World, via POST"
+#   end
 
-  put '/' do
-    "Hello World, via PUT"
-  end
+#   put '/' do
+#     "Hello World, via PUT"
+#   end
 
-  delete '/' do
-    "Goodbye Word, via DELETE"
-  end
+#   delete '/' do
+#     "Goodbye Word, via DELETE"
+#   end
 
-# The following means that just going to '/hello' would fail - as it is only set up for the full path.
-#  get '/hello/:first_name/:last_name' do |first, last|
+# # The following means that just going to '/hello' would fail - as it is only set up for the full path.
+# #  get '/hello/:first_name/:last_name' do |first, last|
+# #    "Hello #{first} #{last}"
+# #  end
+
+# # Add ? to make a part of the filepath optional
+#  get '/hello?/?:first_name?/?:last_name?' do |first, last|
 #    "Hello #{first} #{last}"
 #  end
-
-# Add ? to make a part of the filepath optional
- get '/hello?/?:first_name?/?:last_name?' do |first, last|
-   "Hello #{first} #{last}"
- end
 end
